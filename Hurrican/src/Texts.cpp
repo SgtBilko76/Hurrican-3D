SgtBilko76/Hurrican-3D

commit edf3df906bc71c2b68d02f16c57de0894d42f3cb

Use std::string in place of char arrays, it's safer and fixes some warnings

Hurrican/src/Texts.cpp
@@ -18,6 +18,7 @@
 #include <experimental/filesystem>
 #include <fstream>
 #include <iostream>
+#include <string>
 namespace fs = std::experimental::filesystem::v1;
 
 #include "Gameplay.hpp"
@@ -355,64 +356,64 @@ void InitReplacers() {
         strcpy(s_Replacers[29], GetKeyName(Player[0].AktionKeyboard[AKTION_WAFFE_BOUNCE]));
     } else {
         int joy_idx = Player[0].JoystickIndex;
-        char buf[256];
-        snprintf(buf, 256, "%s %s", TextArray[TEXT_JOYMODE_PAD + Player[0].JoystickMode - 1], TextArray[TEXT_LEFT]);
-        strcpy(s_Replacers[1], buf);
+        std::string buf;
+        buf.append(TextArray[TEXT_JOYMODE_PAD + Player[0].JoystickMode - 1]).append(" ").append(TextArray[TEXT_LEFT]);
+        strcpy(s_Replacers[1], buf.c_str());
 
-        snprintf(buf, 256, "%s %s", TextArray[TEXT_JOYMODE_PAD + Player[0].JoystickMode - 1], TextArray[TEXT_RIGHT]);
-        strcpy(s_Replacers[3], buf);
+        buf.append(TextArray[TEXT_JOYMODE_PAD + Player[0].JoystickMode - 1]).append(" ").append(TextArray[TEXT_RIGHT]);
+        strcpy(s_Replacers[3], buf.c_str());
 
-        snprintf(buf, 256, "%s %s", TextArray[TEXT_JOYMODE_PAD + Player[0].JoystickMode - 1], TextArray[TEXT_DOWN]);
-        strcpy(s_Replacers[5], buf);
+        buf.append(TextArray[TEXT_JOYMODE_PAD + Player[0].JoystickMode - 1]).append(" ").append(TextArray[TEXT_DOWN]);
+        strcpy(s_Replacers[5], buf.c_str());
 
-        snprintf(buf, 256, "%s %s", TextArray[TEXT_JOYMODE_PAD + Player[0].JoystickMode - 1], TextArray[TEXT_UP]);
-        strcpy(s_Replacers[7], buf);
+        buf.append(TextArray[TEXT_JOYMODE_PAD + Player[0].JoystickMode - 1]).append(" ").append(TextArray[TEXT_UP]);
+        strcpy(s_Replacers[7], buf.c_str());
 
-        snprintf(buf, 256, "%s %s", TextArray[TEXT_JOYMODE_PAD + Player[0].JoystickMode - 1], TextArray[TEXT_DOWN]);
-        strcpy(s_Replacers[9], buf);
+        buf.append(TextArray[TEXT_JOYMODE_PAD + Player[0].JoystickMode - 1]).append(" ").append(TextArray[TEXT_DOWN]);
+        strcpy(s_Replacers[9], buf.c_str());
 
         if (Player[0].JoystickMode == JOYMODE_JOYSTICK)
-            snprintf(buf, 256, "%s %s", TextArray[TEXT_JOYMODE_PAD + Player[0].JoystickMode - 1], TextArray[TEXT_UP]);
+            buf.append(TextArray[TEXT_JOYMODE_PAD + Player[0].JoystickMode - 1]).append(" ").append(TextArray[TEXT_UP]);
         else
-            snprintf(buf, 256, "%s %s", TextArray[TEXT_BUTTON],
+            buf.append(TextArray[TEXT_BUTTON]).append(" ").append(
                       DirectInput.MapButtonToString(joy_idx, Player[0].AktionJoystick[AKTION_JUMP]));
 
-        strcpy(s_Replacers[11], buf);
+        strcpy(s_Replacers[11], buf.c_str());
 
-        snprintf(buf, 256, "%s %s", TextArray[TEXT_BUTTON],
+        buf.append(TextArray[TEXT_BUTTON]).append(" ").append(
                   DirectInput.MapButtonToString(joy_idx, Player[0].AktionJoystick[AKTION_SHOOT]));
-        strcpy(s_Replacers[13], buf);
+        strcpy(s_Replacers[13], buf.c_str());
 
-        snprintf(buf, 256, "%s %s", TextArray[TEXT_BUTTON],
+        buf.append(TextArray[TEXT_BUTTON]).append(" ").append(
                   DirectInput.MapButtonToString(joy_idx, Player[0].AktionJoystick[AKTION_BLITZ]));
-        strcpy(s_Replacers[15], buf);
+        strcpy(s_Replacers[15], buf.c_str());
 
-        snprintf(buf, 256, "%s %s", TextArray[TEXT_BUTTON],
+        buf.append(TextArray[TEXT_BUTTON]).append(" ").append(
                   DirectInput.MapButtonToString(joy_idx, Player[0].AktionJoystick[AKTION_POWERLINE]));
-        strcpy(s_Replacers[17], buf);
+        strcpy(s_Replacers[17], buf.c_str());
 
-        snprintf(buf, 256, "%s %s", TextArray[TEXT_BUTTON],
+        buf.append(TextArray[TEXT_BUTTON]).append(" ").append(
                   DirectInput.MapButtonToString(joy_idx, Player[0].AktionJoystick[AKTION_GRANATE]));
-        strcpy(s_Replacers[19], buf);
+        strcpy(s_Replacers[19], buf.c_str());
 
-        snprintf(buf, 256, "%s %s", TextArray[TEXT_BUTTON],
+        buf.append(TextArray[TEXT_BUTTON]).append(" ").append(
                   DirectInput.MapButtonToString(joy_idx, Player[0].AktionJoystick[AKTION_SMARTBOMB]));
-        strcpy(s_Replacers[21], buf);
+        strcpy(s_Replacers[21], buf.c_str());
 
-        snprintf(buf, 256, "%s %s", TextArray[TEXT_BUTTON],
+        buf.append(TextArray[TEXT_BUTTON]).append(" ").append(
                   DirectInput.MapButtonToString(joy_idx, Player[0].AktionJoystick[AKTION_WAFFEN_CYCLE]));
-        strcpy(s_Replacers[23], buf);
+        strcpy(s_Replacers[23], buf.c_str());
 
-        snprintf(buf, 256, "%s %s", TextArray[TEXT_BUTTON],
+        buf.append(TextArray[TEXT_BUTTON]).append(" ").append(
                   DirectInput.MapButtonToString(joy_idx, Player[0].AktionJoystick[AKTION_WAFFE_SPREAD]));
-        strcpy(s_Replacers[25], buf);
+        strcpy(s_Replacers[25], buf.c_str());
 
-        snprintf(buf, 256, "%s %s", TextArray[TEXT_BUTTON],
+        buf.append(TextArray[TEXT_BUTTON]).append(" ").append(
                   DirectInput.MapButtonToString(joy_idx, Player[0].AktionJoystick[AKTION_WAFFE_LASER]));
-        strcpy(s_Replacers[27], buf);
+        strcpy(s_Replacers[27], buf.c_str());
 
-        snprintf(buf, 256, "%s %s", TextArray[TEXT_BUTTON],
+        buf.append(TextArray[TEXT_BUTTON]).append(" ").append(
                   DirectInput.MapButtonToString(joy_idx, Player[0].AktionJoystick[AKTION_WAFFE_BOUNCE]));
-        strcpy(s_Replacers[29], buf);
+        strcpy(s_Replacers[29], buf.c_str());
     }
 }

Hurrican/src/Menu.cpp
@@ -18,6 +18,7 @@
 #endif
 #include <stdio.h>
 #include <time.h>
+#include <string>
 #include "Console.hpp"
 #include "DX8Font.hpp"
 #include "DX8Graphics.hpp"
@@ -828,64 +829,64 @@ void MenuClass::ShowMenu() {
                                                            GetKeyName(pCurrentPlayer->AktionKeyboard[i]), col2);
                             } else {
                                 // Joy axis/hat/button
-                                char Buf[80];
+                                std::string Buf;
                                 if (on_move_line) {
                                     // The three movement assignments (Walk Left, Walk Right, Crouch) can only be
                                     //  assigned to DPAD or X/Y-analog-axis and are a special case:
                                     if (pCurrentPlayer->Walk_UseAxxis) {
                                         // Movement is assigned to analog stick
-                                        strcpy_s(Buf, TextArray[TEXT_JOY_ACHSE]);
+                                        Buf = TextArray[TEXT_JOY_ACHSE];
                                     } else {
                                         // Movement is assigned to D-PAD HAT
-                                        strcpy_s(Buf, TextArray[TEXT_JOY_COOLIE]);
+                                        Buf = TextArray[TEXT_JOY_COOLIE];
                                     }
                                 } else if (on_look_line) {
                                     // Similarly, the two look assignments can only be assigned to DPAD or
                                     // X/Y-analog-axis
                                     if (pCurrentPlayer->Look_UseAxxis) {
                                         // Look up/down is assigned to analog stick
-                                        strcpy_s(Buf, TextArray[TEXT_JOY_ACHSE]);
+                                        Buf = TextArray[TEXT_JOY_ACHSE];
                                     } else {
                                         // Look up/down is assigned to DPAD HAT
-                                        strcpy_s(Buf, TextArray[TEXT_JOY_COOLIE]);
+                                        Buf = TextArray[TEXT_JOY_COOLIE];
                                     }
                                 } else if (on_jump_line && pCurrentPlayer->JoystickMode == JOYMODE_JOYSTICK) {
                                     // When in joystick mode, jump is handled by movement (DPAD or analog axis)
                                     if (pCurrentPlayer->Walk_UseAxxis) {
                                         // Jump/Movement is assigned to analog stick
-                                        strcpy_s(Buf, TextArray[TEXT_JOY_ACHSE]);
+                                        Buf = TextArray[TEXT_JOY_ACHSE];
                                     } else {
                                         // Jump/Movement is assigned to DPAD HAT
-                                        strcpy_s(Buf, TextArray[TEXT_JOY_COOLIE]);
+                                        Buf = TextArray[TEXT_JOY_COOLIE];
                                     }
                                 } else {
                                     // Joy button
-                                    snprintf(Buf, 80, "%s %s", TextArray[TEXT_BUTTON],
+                                    Buf.append(TextArray[TEXT_BUTTON]).append(" ").append(
                                               DirectInput.MapButtonToString(pCurrentPlayer->JoystickIndex,
                                                                             pCurrentPlayer->AktionJoystick[i]));
                                 }
 
                                 pDefaultFont->DrawText(col1_off_x + j * col2_off_x, controls_off_y + i * line_spacing,
-                                                       Buf, col2);
+                                                       Buf.c_str(), col2);
                             }
                         }
                     } else {
-                        char Buf[80];
+                        std::string Buf;
                         // Redefinition prompt
                         if (scale_factor <= 1) {
                             if (pCurrentPlayer->ControlType == CONTROLTYPE_KEYBOARD)
                                 // Ask for new key
-                                strcpy_s(Buf, TextArray[TEXT_TASTEN_NEU_T]);
+                                Buf=TextArray[TEXT_TASTEN_NEU_T];
                             else
                                 // Ask for new button
-                                strcpy_s(Buf, TextArray[TEXT_TASTEN_NEU_B]);
+                                Buf=TextArray[TEXT_TASTEN_NEU_B];
                         } else {
                             // When using scaled fonts (low-res device), there's not enough room for the text prompt
-                            strcpy_s(Buf, "???");
+                            Buf="???";
                         }
 
-                        pDefaultFont->DrawText(col1_off_x + j * col2_off_x, controls_off_y + i * line_spacing, Buf,
-                                               col2);
+                        pDefaultFont->DrawText(col1_off_x + j * col2_off_x, controls_off_y + i * line_spacing,
+                                               Buf.c_str(), col2);
                     }
                 }
             }
@@ -933,18 +934,16 @@ void MenuClass::ShowMenu() {
         } break;  // HIGHSCORE
 
         case MENUZUSTAND_ENTERNAME: {
-            char Buffer[100];  // Für itoa
-
             pMenuFont->DrawTextCenterAlign(320, ypos + 90, TextArray[TEXT_WAHNSINN], D3DCOLOR_RGBA(255, 255, 255, 255),
                                            2);
 
-            strcpy_s(Buffer, strlen(TextArray[TEXT_NEUE_HIGHSCORE]) + 1, TextArray[TEXT_NEUE_HIGHSCORE]);
             pMenuFont->DrawTextCenterAlign(320, ypos + 180, TextArray[TEXT_NAMEN_EINGEBEN],
                                            D3DCOLOR_RGBA(255, 255, 255, 255), 2);
 
-            snprintf(Buffer, 100, "%s %d", Buffer, NewRank + 1);
+            std::string Buffer(TextArray[TEXT_NEUE_HIGHSCORE]);
+            Buffer.append(std::to_string(NewRank + 1));
 
-            pMenuFont->DrawTextCenterAlign(320, ypos + 150, Buffer, D3DCOLOR_RGBA(255, 255, 255, 255), 2);
+            pMenuFont->DrawTextCenterAlign(320, ypos + 150, Buffer.c_str(), D3DCOLOR_RGBA(255, 255, 255, 255), 2);
         } break;  // ENTERNAME
 
         case MENUPUNKT_CREDITS: {

Hurrican/src/Outtro.cpp
@@ -14,6 +14,7 @@
 
 #include "Outtro.hpp"
 #include <stdio.h>
+#include <string>
 #include "Console.hpp"
 #include "DX8Sound.hpp"
 #include "Gameplay.hpp"
@@ -319,7 +320,7 @@ void OuttroClass::DoOuttro() {
             //      Note that, for here, we don't use the LowResCredts[] text array,
             //      as the logic here requires lines to be at specific locations.
             //      We end up with a little jerkiness when lines are split, but that's OK.
-            char text[255];
+            std::string text;
             char text1[255];
             char text2[255];
 
@@ -366,18 +367,16 @@ void OuttroClass::DoOuttro() {
                 int off2 = TextOff + i - 27;
                 if (off2 > 0) {
                     if (TEXT_OUTTRO1 + off2 < TEXT_SEPERATOR_MARIO)
-                        snprintf(text, 255, "%s", TextArray[TEXT_OUTTRO1 + off2]);
+                        text = TextArray[TEXT_OUTTRO1 + off2];
                     else if (TEXT_OUTTRO1 + off2 <= TEXT_SEPERATOR_MARIO + AnzahlCredits)
-                        snprintf(text, 255, "%s", Credits[off2 + 25]);
+                        text = Credits[off2 + 25];
                     else if (TEXT_OUTTRO1 + off2 <= TEXT_SEPERATOR_MARIO + AnzahlCredits + 10)
-                        snprintf(text, 255, "%s", TextArray[TEXT_OUTTRO1 + off2 - AnzahlCredits]);
-                    else
-                        strcpy_s(text, "");
+                        text = TextArray[TEXT_OUTTRO1 + off2 - AnzahlCredits];
 
                     // rendern
-                    if (CommandLineParams.LowRes && strlen(text) > 10 &&
-                        pDefaultFont->StringLength(text, 0) > max_draw_width) {
-                        SplitLine(text1, text2, text);
+                    if (CommandLineParams.LowRes && text.size() > 10 &&
+                        pDefaultFont->StringLength(text.c_str(), 0) > max_draw_width) {
+                        SplitLine(text1, text2, text.c_str());
                         pDefaultFont->DrawTextCenterAlign(320, float(display_line * yoff_inc) - Counter, text1,
                                                           0xFFEEFFFF, 0);
                         display_line++;
@@ -385,8 +384,8 @@ void OuttroClass::DoOuttro() {
                                                           0xFFEEFFFF, 0);
 
                     } else {
-                        if (strcmp(text, "") != 0)
-                            pDefaultFont->DrawTextCenterAlign(320, float(display_line * yoff_inc) - Counter, text,
+                        if (!text.empty())
+                            pDefaultFont->DrawTextCenterAlign(320, float(display_line * yoff_inc) - Counter, text.c_str(),
                                                               0xFFEEFFFF, 0);
                     }
                 }

commit b2cf29bb28f4fb72f54a1e905b4a4565817ff80a

Correct display of gamepad control names in tutorial level, broken with edf3df9 (issue #8)

Hurrican/src/Texts.cpp
@@ -355,62 +355,62 @@ void InitReplacers() {
     } else {
         int joy_idx = Player[0].JoystickIndex;
         std::string buf;
-        buf.append(TextArray[TEXT_JOYMODE_PAD + Player[0].JoystickMode - 1]).append(" ").append(TextArray[TEXT_LEFT]);
+        buf.assign(TextArray[TEXT_JOYMODE_PAD + Player[0].JoystickMode - 1]).append(" ").append(TextArray[TEXT_LEFT]);
         strcpy(s_Replacers[1], buf.c_str());
 
-        buf.append(TextArray[TEXT_JOYMODE_PAD + Player[0].JoystickMode - 1]).append(" ").append(TextArray[TEXT_RIGHT]);
+        buf.assign(TextArray[TEXT_JOYMODE_PAD + Player[0].JoystickMode - 1]).append(" ").append(TextArray[TEXT_RIGHT]);
         strcpy(s_Replacers[3], buf.c_str());
 
-        buf.append(TextArray[TEXT_JOYMODE_PAD + Player[0].JoystickMode - 1]).append(" ").append(TextArray[TEXT_DOWN]);
+        buf.assign(TextArray[TEXT_JOYMODE_PAD + Player[0].JoystickMode - 1]).append(" ").append(TextArray[TEXT_DOWN]);
         strcpy(s_Replacers[5], buf.c_str());
 
-        buf.append(TextArray[TEXT_JOYMODE_PAD + Player[0].JoystickMode - 1]).append(" ").append(TextArray[TEXT_UP]);
+        buf.assign(TextArray[TEXT_JOYMODE_PAD + Player[0].JoystickMode - 1]).append(" ").append(TextArray[TEXT_UP]);
         strcpy(s_Replacers[7], buf.c_str());
 
-        buf.append(TextArray[TEXT_JOYMODE_PAD + Player[0].JoystickMode - 1]).append(" ").append(TextArray[TEXT_DOWN]);
+        buf.assign(TextArray[TEXT_JOYMODE_PAD + Player[0].JoystickMode - 1]).append(" ").append(TextArray[TEXT_DOWN]);
         strcpy(s_Replacers[9], buf.c_str());
 
         if (Player[0].JoystickMode == JOYMODE_JOYSTICK)
-            buf.append(TextArray[TEXT_JOYMODE_PAD + Player[0].JoystickMode - 1]).append(" ").append(TextArray[TEXT_UP]);
+            buf.assign(TextArray[TEXT_JOYMODE_PAD + Player[0].JoystickMode - 1]).append(" ").append(TextArray[TEXT_UP]);
         else
-            buf.append(TextArray[TEXT_BUTTON]).append(" ").append(
+            buf.assign(TextArray[TEXT_BUTTON]).append(" ").append(
                       DirectInput.MapButtonToString(joy_idx, Player[0].AktionJoystick[AKTION_JUMP]));
 
         strcpy(s_Replacers[11], buf.c_str());
 
-        buf.append(TextArray[TEXT_BUTTON]).append(" ").append(
+        buf.assign(TextArray[TEXT_BUTTON]).append(" ").append(
                   DirectInput.MapButtonToString(joy_idx, Player[0].AktionJoystick[AKTION_SHOOT]));
         strcpy(s_Replacers[13], buf.c_str());
 
-        buf.append(TextArray[TEXT_BUTTON]).append(" ").append(
+        buf.assign(TextArray[TEXT_BUTTON]).append(" ").append(
                   DirectInput.MapButtonToString(joy_idx, Player[0].AktionJoystick[AKTION_BLITZ]));
         strcpy(s_Replacers[15], buf.c_str());
 
-        buf.append(TextArray[TEXT_BUTTON]).append(" ").append(
+        buf.assign(TextArray[TEXT_BUTTON]).append(" ").append(
                   DirectInput.MapButtonToString(joy_idx, Player[0].AktionJoystick[AKTION_POWERLINE]));
         strcpy(s_Replacers[17], buf.c_str());
 
-        buf.append(TextArray[TEXT_BUTTON]).append(" ").append(
+        buf.assign(TextArray[TEXT_BUTTON]).append(" ").append(
                   DirectInput.MapButtonToString(joy_idx, Player[0].AktionJoystick[AKTION_GRANATE]));
         strcpy(s_Replacers[19], buf.c_str());
 
-        buf.append(TextArray[TEXT_BUTTON]).append(" ").append(
+        buf.assign(TextArray[TEXT_BUTTON]).append(" ").append(
                   DirectInput.MapButtonToString(joy_idx, Player[0].AktionJoystick[AKTION_SMARTBOMB]));
         strcpy(s_Replacers[21], buf.c_str());
 
-        buf.append(TextArray[TEXT_BUTTON]).append(" ").append(
+        buf.assign(TextArray[TEXT_BUTTON]).append(" ").append(
                   DirectInput.MapButtonToString(joy_idx, Player[0].AktionJoystick[AKTION_WAFFEN_CYCLE]));
         strcpy(s_Replacers[23], buf.c_str());
 
-        buf.append(TextArray[TEXT_BUTTON]).append(" ").append(
+        buf.assign(TextArray[TEXT_BUTTON]).append(" ").append(
                   DirectInput.MapButtonToString(joy_idx, Player[0].AktionJoystick[AKTION_WAFFE_SPREAD]));
         strcpy(s_Replacers[25], buf.c_str());
 
-        buf.append(TextArray[TEXT_BUTTON]).append(" ").append(
+        buf.assign(TextArray[TEXT_BUTTON]).append(" ").append(
                   DirectInput.MapButtonToString(joy_idx, Player[0].AktionJoystick[AKTION_WAFFE_LASER]));
         strcpy(s_Replacers[27], buf.c_str());
 
-        buf.append(TextArray[TEXT_BUTTON]).append(" ").append(
+        buf.assign(TextArray[TEXT_BUTTON]).append(" ").append(
                   DirectInput.MapButtonToString(joy_idx, Player[0].AktionJoystick[AKTION_WAFFE_BOUNCE]));
         strcpy(s_Replacers[29], buf.c_str());
     }
SgtBilko76/Hurrican-3D

commit 202863c0561e2963641e993d5d16865b99a9937b

Use function from the std namespace

Hurrican/src/DX8Sound.cpp
@@ -672,7 +672,7 @@ int SoundManagerClass::PlayWave3D_SDL(int x, int y, int nr) {
     float ydiff = ((Player[0].ypos + 45) - y);
 
     // DKS - converted to float:
-    float Abstand = sqrtf((xdiff * xdiff) + (ydiff * ydiff));
+    float Abstand = std::sqrtf((xdiff * xdiff) + (ydiff * ydiff));
 
     int vol = static_cast<int>(100.0f - Abstand / 6.0f);
     if (vol < 0)

commit b17b61b6575d4e0af2bcdf879d845d98a17a3568

Add missing include
fix build broken with 202863c

Hurrican/src/DX8Sound.cpp
@@ -22,6 +22,7 @@
 // --------------------------------------------------------------------------------------
 
 #include <cstdio>
+#include <cmath>
 #include <filesystem>
 namespace fs = std::filesystem;
 